e0f893f9d26e5b900ac35a5084b11f92826b95a5
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-05-17 19:31:06 -0300

    feat: add and config eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,4 @@
-import fastify from 'fastify';
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello World!!'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('🚀 HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
