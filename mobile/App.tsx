3c06de5d076ab48a1fd04c12809d16096a38ab9c
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-05-31 18:02:39 -0300

    adjusts: initial layout in mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/blurBg.png'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-gray-50">Hello World!</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-950"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
