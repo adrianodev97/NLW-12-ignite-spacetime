9fc78919b728e8c3138fe2df822a9fb327eb9a0c
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-06-08 03:02:44 -0300

    feat: implement new memories page and config route

--- web/src/components/Hero.tsx
+++ web/src/components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import nlwLogo from '../assets/nlw-spacetime-logo.svg'
 import Image from 'next/image'
 
@@ -14,12 +15,12 @@ export const Hero = () => {
           com o mundo!
         </p>
       </div>
-      <a
+      <Link
         className="inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-300"
-        href="#"
+        href="/memories/new"
       >
         CADASTRAR LEMBRANÇA
-      </a>
+      </Link>
     </div>
   )
 }
